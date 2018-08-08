8dc7d31ab4e335de160f5ddd52e06268f2ee43e0
dragonguy00 <42085666+dragonguy00@users.noreply.github.com>
2018-08-08 10:02:33 +1000

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-)}
 
+  
   if (cmd === `${prefix}inactive`) {
 
       //>inactive <duration> <reason>
